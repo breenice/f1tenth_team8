0d636200c899e9f38f90b1f015de28846fa63248
Andrew Ma <44853183+andrewma5@users.noreply.github.com>
2024-10-17 20:26:27 -0400

    Change car 9 to 8

--- src/scan_test.py
+++ src/scan_test.py
@@ -7,5 +7,5 @@ def callback(data):
     print data.ranges[540]
 
 rospy.init_node("scan_test", anonymous=False)
-sub = rospy.Subscriber("/car_1/scan", LaserScan, callback)
+sub = rospy.Subscriber("/car_8/scan", LaserScan, callback)
 rospy.spin()
