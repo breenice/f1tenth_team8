fa1bcfb86ea98b31d7680e094f0a95aafb011a71
Andrew Ma <44853183+andrewma5@users.noreply.github.com>
2024-10-21 01:41:09 -0400

    rviz arrow

--- src/rviz_test.py
+++ src/rviz_test.py
@@ -3,6 +3,7 @@
 import rospy
 import math
 from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDrive
 from std_msgs.msg import String
 from std_msgs.msg import Int32
 
@@ -12,54 +13,55 @@ from tf.transformations import quaternion_from_euler
 
 sphere_marker_pub = rospy.Publisher("/sphere_marker", Marker, queue_size = 2)
 arrow_marker_pub = rospy.Publisher("/arrow_marker", Marker, queue_size = 2)
-prev_range = 0.0
 
 
 def callback(data):
     
-    global prev_range
-    # pick out the middle range value (at index 540 - just an example)
-    center = data.ranges[540]
+    # global prev_range
+    # # pick out the middle range value (at index 540 - just an example)
+    # center = data.ranges[540]
 
-    # If NaN, pipe though the previous non NaN value
-    if math.isnan(center):
-        center = prev_range
+    # # If NaN, pipe though the previous non NaN value
+    # if math.isnan(center):
+    #     center = prev_range
     
-    prev_range = center
-    mid = "Middle Range %s" % (center)
-    rospy.loginfo(mid)
+    # prev_range = center
+    # mid = "Middle Range %s" % (center)
+    # rospy.loginfo(mid)
+
+    steering_angle = data.steering_angle
 
 
-    # SPHERE MARKER
+    # # SPHERE MARKER
 
-    sphere_marker = Marker()
+    # sphere_marker = Marker()
 
-    sphere_marker.header.frame_id = "car_8_laser"
-    sphere_marker.header.stamp = rospy.Time.now()
+    # sphere_marker.header.frame_id = "car_8_laser"
+    # sphere_marker.header.stamp = rospy.Time.now()
 
-    # set shape, Arrow: 0; Cube: 1 ; Sphere: 2 ; Cylinder: 3
-    sphere_marker.type = 2
-    sphere_marker.id = 0
+    # # set shape, Arrow: 0; Cube: 1 ; Sphere: 2 ; Cylinder: 3
+    # sphere_marker.type = 2
+    # sphere_marker.id = 0
 
-    # Set the scale of the marker
-    sphere_marker.scale.x = 0.2
-    sphere_marker.scale.y = 0.2
-    sphere_marker.scale.z = 0.2
+    # # Set the scale of the marker
+    # sphere_marker.scale.x = 0.2
+    # sphere_marker.scale.y = 0.2
+    # sphere_marker.scale.z = 0.2
 
-    # Set the color
-    sphere_marker.color.r = 0.0
-    sphere_marker.color.g = 1.0
-    sphere_marker.color.b = 0.0
-    sphere_marker.color.a = 1.0
+    # # Set the color
+    # sphere_marker.color.r = 0.0
+    # sphere_marker.color.g = 1.0
+    # sphere_marker.color.b = 0.0
+    # sphere_marker.color.a = 1.0
 
-    # Set the pose of the marker
-    sphere_marker.pose.position.x = center
-    sphere_marker.pose.position.y = 0
-    sphere_marker.pose.position.z = 0
-    sphere_marker.pose.orientation.x = 0.0
-    sphere_marker.pose.orientation.y = 0.0
-    sphere_marker.pose.orientation.z = 0.0
-    sphere_marker.pose.orientation.w = 1.0
+    # # Set the pose of the marker
+    # sphere_marker.pose.position.x = center
+    # sphere_marker.pose.position.y = 0
+    # sphere_marker.pose.position.z = 0
+    # sphere_marker.pose.orientation.x = 0.0
+    # sphere_marker.pose.orientation.y = 0.0
+    # sphere_marker.pose.orientation.z = 0.0
+    # sphere_marker.pose.orientation.w = 1.0
 
     # Convert steering Euler angle (yaw) to quaternion
     #example_steering = 10
@@ -69,7 +71,7 @@ def callback(data):
     #sphere_marker.pose.orientation.z = quaternion[2]
     #sphere_marker.pose.orientation.w = quaternion[3]
 
-    sphere_marker_pub.publish(sphere_marker)
+    # sphere_marker_pub.publish(sphere_marker)
 
     # ARROW MARKER
 
@@ -80,7 +82,13 @@ def callback(data):
     arrow_marker.header.stamp = rospy.Time.now()
     arrow_marker.id = 1
 
-    arrow_marker.scale.x = data.ranges[540]
+    quaternion = quaternion_from_euler(0,0,steering_angle) # args: Roll, Pitch, Yaw
+    arrow_marker.pose.orientation.x = quaternion[0]
+    arrow_marker.pose.orientation.y = quaternion[1]
+    arrow_marker.pose.orientation.z = quaternion[2]
+    arrow_marker.pose.orientation.w = quaternion[3]
+
+    arrow_marker.scale.x = 0.1 # data.ranges[540]
     arrow_marker.scale.y = 0.1
     arrow_marker.scale.z = 0.1
 
@@ -95,6 +103,7 @@ def callback(data):
    
 if __name__=='__main__':
     rospy.init_node("rviz_test", anonymous=False)   
-    sub = rospy.Subscriber("/car_8/scan",LaserScan, callback)
+    # sub = rospy.Subscriber("/car_8/scan",LaserScan, callback)
+    sub = rospy.Subscriber("/car_8/offboard/command",AckermannDrive, callback)
     rospy.spin()
 
