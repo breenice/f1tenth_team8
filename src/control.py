f9aa72ae3e8fe0dc25f08302eba28046e15626bc
jeremyky <anderw271314@gmail.com>
2024-10-20 21:58:51 -0400

    changes

--- src/control.py
+++ src/control.py
@@ -7,9 +7,9 @@ from ackermann_msgs.msg import AckermannDrive
 from collections import deque
 
 # PID Control Params
-kp = 0.0 #TODO
-kd = 0.0 #TODO
-ki = 0.0 #TODO
+# kp = 0.0 #TODO
+# kd = 0.0 #TODO
+# ki = 0.0 #TODO
 servo_offset = 0.0	# zero correction offset in case servo is misaligned and has a bias in turning.
 prev_error = 0.0
 
@@ -26,7 +26,7 @@ INTEGRAL_LENGTH = 10
 # 25: Slow and steady
 # 35: Nice Autonomous Pace
 # > 40: Careful, what you do here. Only use this if your autonomous steering is very reliable.
-vel_input = 0.0	#TODO
+# vel_input = 0.0	#TODO
 
 # Publisher for moving the car.
 command_pub = rospy.Publisher('/car_8/offboard/command', AckermannDrive, queue_size = 1)
@@ -34,6 +34,7 @@ command_pub = rospy.Publisher('/car_8/offboard/command', AckermannDrive, queue_s
 def control(data):
 	global prev_error
 	global vel_input
+	global total_error
 	global kp, kd, ki
 
 	print("PID Control Node is Listening to error")
@@ -42,11 +43,12 @@ def control(data):
 	#TODO: Use kp, ki & kd to implement a PID controller
 
 	# 1. Scale the error
-	error = E * data.error
+	E = -1
+	error = E * data.pid_error
 
 	# Bookkeeping for integral value
 	errors.append(error)
-	total_error += errors
+	total_error += error
 	if len(errors) > INTEGRAL_LENGTH:
 		total_error -= errors.popleft()
 
@@ -56,12 +58,14 @@ def control(data):
 	I = total_error
 	D = prev_error - error
 
-	angle = kp * P + ki * I + kd * D
+	angle = kp * P # + ki * I + kd * D
 
 	# An empty AckermannDrive message is created. You will populate the steering_angle and the speed fields.
 	command = AckermannDrive()
 
 	# TODO: Make sure the steering value is within bounds [-100,100]
+	print("Error: " + str(data.pid_error))
+	print("Steering Angle: " + str(angle))
 	command.steering_angle = max(min(100, angle), -100)
 
 	# TODO: Make sure the velocity is within bounds [0,100]
@@ -73,14 +77,12 @@ def control(data):
 if __name__ == '__main__':
 
     # This code tempalte asks for the values for the gains from the user upon start, but you are free to set them as ROS parameters as well.
-	global kp
-	global kd
-	global ki
+	global kp, kd, ki
 	global vel_input
-	kp = input("Enter Kp Value: ")
-	kd = input("Enter Kd Value: ")
-	ki = input("Enter Ki Value: ")
-	vel_input = input("Enter desired velocity: ")
+	kp = 10 # input("Enter Kp Value: ")
+	kd = 0 # input("Enter Kd Value: ")
+	ki = 0 # input("Enter Ki Value: ")
+	vel_input = 15 # input("Enter desired velocity: ")
 	rospy.init_node('pid_controller', anonymous=True)
     # subscribe to the error topic
 	rospy.Subscriber("error", pid_input, control)
--- src/dist_finder.py
+++ src/dist_finder.py
@@ -14,74 +14,87 @@ vel = 15 		# this vel variable is not really used here.
 error = 0.0		# initialize the error
 car_length = 0.50 # Traxxas Rally is 20 inches or 0.5 meters. Useful variable.
 
-LIDAR_MAX_RANGE = 4 # Maximum range of LIDAR scan
+LIDAR_MAX_RANGE = 400 # Maximum range of LIDAR scan
 
 # Handle to the publisher that will publish on the error topic, messages of the type 'pid_input'
 pub = rospy.Publisher('error', pid_input, queue_size=10)
 
 
 def getRange(data, angle):
-	# data: single message from topic /scan
+    # data: single message from topic /scan
     # angle: between -30 to 210 degrees, where 0 degrees is directly to the right, and 90 degrees is directly in front
     # Outputs length in meters to object with angle in lidar scan field of view
     # Make sure to take care of NaNs etc.
- 	#TODO: implement
+     #TODO: implement
 
- 	if not -30 <= angle <= 210:
- 		raise ValueError(f"Angle {angle} out of bounds")
+    #  if not -30 <= angle <= 210:
+    #     raise ValueError "Angle " + str(angle) + " out of bounds"
 
- 	# Convert to radians because LaserScan is in radians
- 	angle_rad = math.radians(angle)
+     # Convert to radians because LaserScan is in radians
+    angle_rad = math.radians(angle)
         
     index = int((angle_rad - data.angle_min) / data.angle_increment)
     
     range_value = data.ranges[index]
-    
-    if np.isnan(range_value):
-        range_value = LIDAR_MAX_RANGE
 
-    range_value = max(range_value, LIDAR_MAX_RANGE)
+    offset = 1
+    while np.isnan(range_value):
+        range_value = data.ranges[index + offset]
+        if offset > 0:
+            offset *= -1
+        else:
+            offset *= -1
+            offset += 1
+        
+        if offset > 15: break
+
+    # range_value = min(range_value, LIDAR_MAX_RANGE)
     
     return range_value
 
 
 
 def callback(data):
-	global forward_projection
+    global forward_projection
 
-	thetas = [50] # you need to try different values for theta
-	# ^ can add diff values of theta later and calculate average or try diff values of theta
+    theta = 30 # you need to try different values for theta
+    # ^ can add diff values of theta later and calculate average or try diff values of theta
 
-	# testing multiple thetas
-	for theta in thetas:
-		a = getRange(data,theta) # obtain the ray distance for theta
-		b = getRange(data,0)	# obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
-		swing = math.radians(theta)
+    # testing multiple thetas
+    a = getRange(data,theta) # obtain the ray distance for theta
+    b = getRange(data,0)	# obtain the ray distance for 0 degrees (i.e. directly to the right of the car)
+    swing = math.radians(theta)
 
-		# ----------------------------------------------
-		
-		## Your code goes here to determine the projected error as per the alrorithm
-		# Compute Alpha, AB, and CD..and finally the error.
-		alpha = math.atan((a * math.cos(swing) - b) / (a * math.sin(swing)))
-		AB = b * math.cos(alpha)
+    # ----------------------------------------------
+    
+    ## Your code goes here to determine the projected error as per the alrorithm
+    # Compute Alpha, AB, and CD..and finally the error.
+    alpha = math.atan((a * math.cos(swing) - b)/ (a * math.sin(swing)))
+    AB = b * math.cos(alpha)
 
-		CD = AB + (forward_projection + car_length) * math.sin(alpha) # forward projection is AC
-		# try w/ (forward_projection) * math.sin(alpha) if bad results
-		error += desired_distance  - CD
-	error = error / len(thetas) # average error over all thetas
-		
-		# ----------------------------------------------
+    CD = AB + (forward_projection + car_length) * math.sin(alpha) # forward projection is AC
+    # try w/ (forward_projection + car_length) * math.sin(alpha) if bad results
+    error = desired_distance - CD
 
-	msg = pid_input()	# An empty msg is created of the type pid_input
-	# this is the error that you want to send to the PID for steering correction.
-	msg.pid_error = error
-	msg.pid_vel = vel		# velocity error can also be sent.
-	pub.publish(msg)
+    print("Angle " + str(theta) + ": " + str(a))
+    print("Angle " + str(0) + ": " + str(b))
+    print("alpha: " + str(alpha))
+    print("AB: " + str(AB) + ", CD: " + str(CD))    
+    print("Error: " + str(error))
+
+        
+    # ----------------------------------------------
+
+    msg = pid_input()	# An empty msg is created of the type pid_input
+    # this is the error that you want to send to the PID for steering correction.
+    msg.pid_error = error
+    msg.pid_vel = vel		# velocity error can also be sent.
+    pub.publish(msg)
 
 
 if __name__ == '__main__':
-	print("Hokuyo LIDAR node started")
-	rospy.init_node('dist_finder',anonymous = True)
-	# TODO: Make sure you are subscribing to the correct car_x/scan topic on your racecar
-	rospy.Subscriber("/car_8/scan",LaserScan,callback)
-	rospy.spin()
+    print("Hokuyo LIDAR node started")
+    rospy.init_node('dist_finder',anonymous = True)
+    # TODO: Make sure you are subscribing to the correct car_x/scan topic on your racecar
+    rospy.Subscriber("/car_8/scan",LaserScan,callback)
+    rospy.spin()
