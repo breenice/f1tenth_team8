ee1dbe37b778d00930fd740e9054690e4b6bb237
Diana T Nguyen <143566648+dianatuyen@users.noreply.github.com>
2024-11-05 18:49:04 -0500

    Update disparity_extension.py

--- src/disparity_extension.py
+++ src/disparity_extension.py
@@ -1,5 +1,4 @@
 
-
 class DisparityExtender:
     def __init__(self, safety_radius=0.5):
         self._safety_radius = safety_radius
@@ -8,11 +7,17 @@ class DisparityExtender:
     #  distance of the obstacle, not 0
     def safety_bubble(self, ranges, closest_idx, angle_increment):
         """
-        apply a safety bubble around disparities to avoid collisions
+        apply a safety bubble around all obstacles detected
         """
-        # calculate the start and end indices for the safety bubble
-        start = max(0, closest_idx - int(self._safety_radius / angle_increment))
-        end = min(len(ranges), closest_idx + int(self._safety_radius / angle_increment))
+        bubble_range = int(self.safety_radius / angle_increment)
 
-        # mark all points within the safety bubble as obstacles (set them to 0)
-        ranges[start:end] = 0
+        for i in range(1, len(ranges)):
+            if abs(ranges[i] - ranges[i - 1]) > self.safety_radius:
+                start_idx = max(0, i - bubble_range)
+                end_idx = min(len(ranges), i + bubble_range)
+
+                #extend bubble area
+
+                obstacle_distance = ranges[i] 
+                for j in range(start_idx, end_idx):
+                    ranges[j] = min(ranges[j], obstacle_distance)
