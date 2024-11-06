e041a799cadd8d24ac16956842e0fa53e7b685b1
ven7sg <143018944+ven7sg@users.noreply.github.com>
2024-11-05 19:26:25 -0500

    instead of extending in both directions, changed to extend one direction at a time

--- src/disparity_extension.py
+++ src/disparity_extension.py
@@ -1,23 +1,37 @@
-
 class DisparityExtender:
-    def __init__(self, safety_radius=0.5):
+    def __init__(self, safety_radius=0.5, max_range=3):
         self._safety_radius = safety_radius
+        self._max_range = max_range
 
-    # TODO: Safety bubble should extend outwards in one direction from the disparity, and the value should be the
-    #  distance of the obstacle, not 0
-    def safety_bubble(self, ranges, closest_idx, angle_increment):
-        """
-        apply a safety bubble around all obstacles detected
-        """
-        bubble_range = int(self.safety_radius / angle_increment)
+    def preprocess_lidar(self, ranges):
+        # just to make sure all ranges capped to max
+        return [min(r, self._max_range) if r > 0 else self._max_range for r in ranges]
 
-        for i in range(1, len(ranges)):
-            if abs(ranges[i] - ranges[i - 1]) > self.safety_radius:
+    def safety_bubble(self, ranges, angle_increment):
+        """
+        apply a safety bubble around all obstacles detected'
+        -- instead of extending from both directions, we now process range twice: 
+        right (extending left) and left (extending right)
+        """
+        # calculate how many indices to include in the safety bubble
+        bubble_range = int(self._safety_radius / angle_increment)
+
+        # process from right, extend bubble to left
+        for i in range(len(ranges) - 2, -1, -1): # to avoid index error 
+            # detect disparity by checking if the distance change is greater than the safety radius
+            if abs(ranges[i] - ranges[i + 1]) > self._safety_radius:
                 start_idx = max(0, i - bubble_range)
+                obstacle_distance = ranges[i]
+                # extend the bubble left by setting points within range to the obstacle distance
+                for j in range(i, start_idx - 1, -1):
+                    ranges[j] = min(ranges[j], obstacle_distance)
+
+        # process from left, extend bubble to right
+        for i in range(1, len(ranges)):
+            # detect a disparity ^ same way
+            if abs(ranges[i] - ranges[i - 1]) > self._safety_radius:
                 end_idx = min(len(ranges), i + bubble_range)
-
-                #extend bubble area
-
-                obstacle_distance = ranges[i] 
-                for j in range(start_idx, end_idx):
+                obstacle_distance = ranges[i]
+                # extend right 
+                for j in range(i, end_idx):
                     ranges[j] = min(ranges[j], obstacle_distance)
