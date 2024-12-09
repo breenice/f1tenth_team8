1556dc37793d6868eb871b3e7c3437fd5d8303fb
Bee <breenicelee@gmail.com>
2024-12-09 18:09:16 -0500

    start sector building

---
+++ src/final_race/build_sectors/print_idx.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import csv
+import math
+from pp_config import *
+import pure_pursuit as pp
+from pure_pursuit import PurePursuit, PurePursuitControl
+import numpy as np
+
+class PrintIdx(PurePursuit, PurePursuitControl):
+    def __init__(self):
+        super().__init__()
+
+
+    def print_idx(self, data):
+        curr_x = data.pose.position.x
+        curr_y = data.pose.position.y
+        distances = [np.sqrt((x - curr_x)**2 + (y - curr_y)**2) for x, y in self.plan]
+        closest_index = np.argmin(distances)
+        print(closest_index)
+        return closest_index
