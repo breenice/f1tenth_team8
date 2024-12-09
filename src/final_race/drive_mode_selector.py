49d9db995b44558ff24e11a9c379ff92700a4175
Andrew Ma <44853183+andrewma5@users.noreply.github.com>
2024-12-08 19:36:17 -0500

    :monkey:

--- src/final_race/drive_mode_selector.py
+++ src/final_race/drive_mode_selector.py
@@ -1,12 +1,37 @@
 import rospy
+import cv2
+import numpy as np
 from std_msgs.msg import Int32, String
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped
 from overtaker_config import *
 
 class DriveModeSelector:
     def __init__(self):
         self.drive_mode_pub = rospy.Publisher('/{}/drive_mode'.format(CAR_NAME), Int32, queue_size=1)
         self.raceline_pub = rospy.Publisher('/{}/raceline'.format(CAR_NAME), String, queue_size=1)
-        # TODO : Subscribe to lidar or other sensors to determine drive mode
+        
+        # self.map = cv2.imread('base_map.pgm', cv2.IMREAD_GRAYSCALE)
+        
+        self.current_pose = None
+        rospy.Subscriber('/{}/particle_filter/viz/inferred_pose'.format(CAR_NAME), 
+                        PoseStamped, self.pose_callback)
+        
+        rospy.Subscriber("/car_8/scan", LaserScan, self.scan_callback)
+
+    def pose_callback(self, msg):
+        self.current_pose = msg.pose
+
+    def scan_callback(self, scan):
+        """
+        TODO: Based on scan and pose, determine what coordinates have obstacles
+            This can be done by checking if the points on scan + pose are close to walls on the map
+            ie. if the distance between the point and the nearest wall is greater than some threshold, then it is an obstacle
+        Based on these coordinates...
+        Choose a drive mode
+        Or, iterate over racelines and choose raceline that does not have obstacle
+        """
+        pass
 
     def set_mode_stop(self):
         self.drive_mode_pub.publish(DriveMode.STOP)
--- src/final_race/overtaker_config.py
+++ src/final_race/overtaker_config.py
@@ -9,7 +9,7 @@ class SpeedMode:
     STOP = 0
     FTG = 1
     PP = 2
-    CC = 3
+    CC = 3 # Cruise Control
 
 # Racelines are the paths that the overtaker can choose between
 RACELINES = []
